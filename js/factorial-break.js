100c16f35e67a7eeedbfa773c4008236e49a58f8
Fran150620 <fran1506alcorta@gmail.com>
2022-12-29 17:19:23 -0300

    finalizado

---
+++ js/factorial-break.js
@@ -0,0 +1,8 @@
+let total=10;
+let i= total-1;
+while(total){
+    total =total*i;
+    i--;  
+    if (i <= 1) break;
+} 
+console.log(total);
---
+++ js/factorial-for.js
@@ -0,0 +1,5 @@
+let total= 1;
+for(i=1;i<=10;i++){
+    total =total*i;
+}
+console.log(total);
